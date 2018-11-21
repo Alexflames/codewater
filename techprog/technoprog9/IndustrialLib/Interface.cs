33126f21dccaa6e242637d9e441c156dffac2b17
Sadpotato <alexprgrigoriev@gmail.com>
2018-11-21 22:15:15 +0400

    l-9 added

---
+++ techprog/technoprog9/IndustrialLib/Interface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndustrialLib
+{
+    interface IRepairable
+    {
+        float Wear { get; set; }
+
+        int RepairCost();
+
+        void Repair(int MoneyToPay);
+    }
+
+    interface IFuelRunning
+    {
+        float FuelMeter { get; set; }
+
+        float MaxFuel { get; }
+
+        void FillUpFuel(float volume);
+    }
+}
---
+++ techprog/technoprog9/technoprog9-3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace technoprog9_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем пустой поток в памяти  
+            MemoryStream ms = new MemoryStream();
+            byte[] memData = Encoding.ASCII.GetBytes
+                  ("This will go in Memory!");
+            // Записываем данные в память
+            ms.Write(memData, 0, memData.Length);
+            // Устанавливаем указатель на начало 
+            ms.Position = 0;
+            byte[] inData = new byte[100];
+            // Читаем из памяти и выводим на экран
+            ms.Read(inData, 0, 100);
+            Console.WriteLine(Encoding.ASCII.GetString(inData));
+            
+            inData = new byte[100];
+            Console.WriteLine(ms.Length);
+            var length = ms.Length;
+            for (int i = 0; i < length; i+=2)
+            {
+                ms.Write(memData, i, 1);
+            }
+
+            ms.Read(inData, 0, 100);
+
+            inData = new byte[100];
+            for (int i = (int)length - 1; i >= 0; i--)
+            {
+                ms.Write(memData, i, 1);
+            }
+
+            ms.Read(inData, 0, 100);
+
+            // А также переписываем содержимое памяти в файл
+            Stream strm = new FileStream("Memoutput.txt",
+            FileMode.OpenOrCreate, FileAccess.Write);
+            ms.WriteTo(strm);
+        }
+    }
+}
