d6ab50767a30f26880a553aaacabb9122e449a39
Alexflames <alexprgrigoriev@gmail.com>
2019-04-21 16:23:19 +0400

    mpi 10-11

--- MPIApp/MPIApp/MPIApp.cpp
+++ MPIApp/MPIApp/MPIApp.cpp
@@ -3,30 +3,180 @@
 
 #include "pch.h"
 #include "mpi.h"
+#include <ctime>
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-	int Size, Rank;
-	MPI_Init(NULL, NULL);
-	MPI_Comm_size(MPI_COMM_WORLD, &Size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &Rank);
-	cout << "Total number of processes: " << Size
-		<< " Process identifier: " << Rank << endl;
-	MPI_Finalize();
-	system("pause");
-	return 0;
+// Число pi
+#define PI 3.1415926535897932384626433832795
+
+int ProcId, ProcSize;
+
+double fun(double x) {
+	return 1 / sqrt(1 - x * x);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+double funZnam(double a1, double b1, double a2, double b2) {
+	return (b1 - a1) * (b2 - a2);
+}
+
+double funI2(double znam, double sumI1, double sumPix, double y) {
+	return sumI1 * (exp(sin(sumPix) * cos(PI * y)) + 1) / znam;
+}
+
+// Вычисляет степень экспоненты которая связана с x
+double funPix(double x) {
+	return PI * x;
+}
+
+double fun2(double x, double y, double znam) {
+	return (exp(sin(PI * x) * cos(PI * y)) + 1) / znam;
+}
+
+//void doubleIntegral(const double a1, const double b1,
+//    const double a2, const double b2,
+//    const double h, double *res)
+//{
+//    int i, j, n1, n2;
+//    double sumI1, sumPix, sum; // локальная переменная для подсчета интеграла
+//    double x, y; // координата точки сетки
+//    n1 = (int)((b1 - a1) / h); // количество точек сетки интегрирования
+//    n2 = (int)((b2 - a2) / h); // количество точек сетки интегрирования
+//    sum = sumPix = sumI1 = 0.0;
+//#pragma omp parallel for private(x) reduction(+: sumI1, sumPix)
+//    for (j = 0; j < n1; j++)
+//    {
+//        x = a1 + j * h + h / 2.0;
+//        sumI1 += funI1(a1, b1, a2, b2) * h;
+//        sumPix += funPix(x) * h;
+//    }
+//#pragma omp parallel for private(y) reduction(+: sum)
+//    for (i = 0; i < n2; i++) {
+//        y = a2 + i * h + h / 2.0;
+//        sum += funI2(sumI1, sumPix, y) * h;
+//    }
+//    *res = sum;
+//}
+
+void doubleIntegral(const double a1, const double b1,
+	const double a2, const double b2,
+	const double h, double *res)
+{
+	int i, j, n1, n2;
+	double sumI1, sumPix, sum; // локальная переменная для подсчета интеграла
+	double x, x2, y, y2; // координата точки сетки
+	n1 = (int)((b1 - a1) / h); // количество точек сетки интегрирования
+	n2 = (int)((b2 - a2) / h); // количество точек сетки интегрирования
+	sum = sumPix = sumI1 = 0.0;
+	double znam = funZnam(a1, b1, a2, b2);
+//#pragma omp parallel for private(x, x2, y, y2) reduction(+: sum)
+	for (j = ProcId; j < n1; j += ProcSize)
+	{
+		x = a1 + j * h;
+		x2 = x + h;
+		//#pragma omp parallel for private(y) reduction(+:sum)
+		for (i = 0; i < n2; i++) {
+			y = a2 + i * h;
+			y2 = y + h;
+			double f1 = fun2(x, y, znam) * h * h;
+			double f2 = fun2(x2, y2, znam) * h * h;
+			sum += (f1 + f2) / 2.0;
+		}
+	}
+	//#pragma omp parallel for private(y) reduction(+: sum)
+	MPI_Reduce(&sum, res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	//*res = sum;
+}
+
+void integral(const double a, const double b,
+	const double h, double *res)
+{
+	int i, n;
+	double sum; // локальная переменная для подсчета интеграла
+	double x; // координата точки сетки
+	n = (int)((b - a) / h); // количество точек сетки интегрирования
+	sum = 0.0;
+//#pragma omp parallel for private(x) reduction(+: sum)  
+	for (i = ProcId; i < n; i += ProcSize)
+	{
+		x = a + i * h + h / 2.0;
+		sum += fun(x) * h;
+	}
+	MPI_Reduce(&sum, res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+}
+
+void integralSimpson(const double a, const double b,
+	const double h, double *res)
+{
+	int i, n;
+	double sum1, sum2; // локальная переменная для подсчета интеграла
+	double x; // координата точки сетки
+	n = (int)((b - a) / (h * 2)); // количество точек сетки интегрирования
+	sum1 = sum2 = 0.0;
+//#pragma omp parallel for reduction(+: sum1)
+	for (i = ProcId + 1; i <= n; i += ProcSize)
+	{
+		sum1 += fun(a + (2 * i - 1) * h);
+	}
+//#pragma omp parallel for reduction(+: sum2)  
+	for (i = ProcId; i < n; i += ProcSize)
+	{
+		sum2 += fun(a + 2 * i * h);
+	}
+	double sum3 = (h / 3) * (fun(a) + fun(b) + 4 * sum1 + 2 * sum2);
+	MPI_Reduce(&sum3, res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+}
+
+
+double experiment(double *res)
+{
+	double stime, ftime; // время начала и конца расчета
+	double a = 0.0; // левая граница интегрирования
+	double b = 0.99999999; // правая граница интегрирования
+	double h = 0.01; // шаг интегрирования
+	stime = clock();
+	//integral(a, b, h, res); // вызов функции интегрирования
+	//integralSimpson(a, b, h, res); // вызов функции интегрирования
+	doubleIntegral(0, 16, 0, 16, h, res); // вызов функции интегрирования
+	ftime = clock();
+	return (ftime - stime) / CLOCKS_PER_SEC;
+}
+
+int main()
+{
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcId);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcSize);
+	int i; // переменная цикла
+	double time; // время проведенного эксперимента
+	double res;// = 0; // значение вычисленного интеграла
+	double min_time; // минимальное время работы
+	// реализации алгоритма
+	double max_time; // максимальное время работы
+	// реализации алгоритма
+	double avg_time; // среднее время работы
+	// реализации алгоритма
+	int numbExp = 10; // количество запусков программы
+	// первый запуск
+	min_time = max_time = avg_time = experiment(&res);
+	// оставшиеся запуски
+	for (i = 0; i < numbExp - 1; i++)
+	{
+		time = experiment(&res);
+		avg_time += time;
+		if (max_time < time) max_time = time;
+		if (min_time > time) min_time = time;
+	}
+	
+	if (ProcId == 0) {
+		// вывод результатов эксперимента
+		cout << "execution time : " << avg_time / numbExp << "; " <<
+			min_time << "; " << max_time << endl;
+		cout.precision(8);
+		cout << "integral value : " << res << endl;
+	}
+	MPI_Finalize();
+	return 0;
+}
---
+++ omp/SerialGauss/FFTCalculation/FFTCalc.cpp
@@ -0,0 +1,294 @@
+// SerialFFT.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <time.h>
+#include <omp.h>
+using namespace std;
+
+#define PI (3.14159265358979323846)
+
+void BitReversing(complex<double> *inputSignal,
+	complex<double> *outputSignal, int size) {
+	int bitsCount = 0;
+	//bitsCount = log2(size)
+	for (int tmp_size = size; tmp_size > 1; tmp_size /= 2, bitsCount++);
+	//ind - index in input array
+	//revInd - correspondent to ind index in output array
+	int ind;
+//#pragma omp parallel for
+	for (ind = 0; ind < size; ind++)
+	{
+		int mask = 1 << (bitsCount - 1);
+		int revInd = 0;
+		for (int i = 0; i < bitsCount; i++) //bit-reversing
+		{
+			bool val = ind & mask;
+			revInd |= val << i;
+			mask = mask >> 1;
+		}
+		outputSignal[revInd] = inputSignal[ind];
+	}
+}
+
+//void BitReversing(complex<double> *inputSignal,
+//    complex<double> *outputSignal, int size) {
+//    int j = 0, i = 0;
+//#pragma omp parallel for reduction(+: j)
+//    for (i = 0; i < size; i++)
+//    {
+//        if (j > i)
+//        {
+//            outputSignal[i] = inputSignal[j];
+//            outputSignal[j] = inputSignal[i];
+//        }
+//        else
+//            if (j == i)
+//                outputSignal[i] = inputSignal[i];
+//        int m = size >> 1;
+//        while ((m >= 1) && (j >= m))
+//        {
+//            j += -m;
+//            m = m >> 1;
+//        }
+//        j += m;
+//    }
+//}
+
+__inline void Butterfly(complex<double> *signal,
+	complex<double> u, int offset, int butterflySize) {
+	complex<double> tem = signal[offset + butterflySize] * u;
+	signal[offset + butterflySize] = signal[offset] - tem;
+	signal[offset] += tem;
+}
+
+void ParallelFFTCalculation(complex<double> *signal, int size) {
+	int m = 0;
+	int p, i;
+	for (int tmp_size = size; tmp_size > 1; tmp_size /= 2, m++);
+	for (p = 0; p < m; p++)
+	{
+		int butterflyOffset = 1 << (p + 1);
+		int butterflySize = butterflyOffset >> 1;
+		double coeff = PI / butterflySize;
+#pragma omp parallel for schedule(static)
+		for (i = 0; i < size / butterflyOffset; i++)
+			for (int j = 0; j < butterflySize; j++)
+				Butterfly(signal, complex<double>(cos(-j * coeff),
+					sin(-j * coeff)), j + i * butterflyOffset, butterflySize);
+	}
+}
+// FFT computation
+void ParallelFFT(complex<double> *inputSignal,
+	complex<double> *outputSignal, int size) {
+	BitReversing(inputSignal, outputSignal, size);
+	ParallelFFTCalculation(outputSignal, size);
+}
+void PrintSignal(complex<double> *signal, int size) {
+	cout << "Result signal" << endl;
+	for (int i = 0; i < size; i++)
+		cout << signal[i] << endl;
+}
+
+#define PI (3.14159265358979323846)
+//Function for simple initialization of input signal elements
+void DummyDataInitialization(complex<double>* mas, int size) {
+	for (int i = 0; i < size; i++)
+		mas[i] = 0;
+	mas[size - size / 4] = 1;
+}
+//Function for memory allocation and data initialization
+void ProcessInitialization(complex<double>* &inputSignal,
+	complex<double>* &outputSignal, int &size) {
+	// Setting the size of signals
+	do
+	{
+		cout << "Enter the input signal length: ";
+		cin >> size;
+		if (size < 4)
+			cout << "Input signal length should be >= 4" << endl;
+		else
+		{
+			int tmpSize = size;
+			while (tmpSize != 1)
+			{
+				if (tmpSize % 2 != 0)
+				{
+					cout << "Input signal length should be powers of two" << endl;
+					size = -1;
+					break;
+				}
+				tmpSize /= 2;
+			}
+		}
+	} while (size < 4);
+	cout << "Input signal length = " << size << endl;
+	inputSignal = new complex<double>[size];
+	outputSignal = new complex<double>[size];
+	//Initialization of input signal elements - tests
+	DummyDataInitialization(inputSignal, size);
+	//Computational experiments
+	//RandomDataInitialization(inputSignal, size);
+}
+//Function for computational process temination
+void ProcessTermination(complex<double>* &inputSignal,
+	complex<double>* &outputSignal) {
+	delete[] inputSignal;
+	inputSignal = NULL;
+	delete[] outputSignal;
+	outputSignal = NULL;
+}
+
+// Function for random initialization of objects' elements
+void RandomDataInitialization(complex<double>* mas, int size)
+{
+	srand(unsigned(clock()));
+	for (int i = 0; i < size; i++)
+		mas[i] = complex<double>(rand() / 1000.0, rand() / 1000.0);
+}
+
+// S E R I A L      S E R I A L      
+void SerialFFTCalculation(complex<double> *signal, int size) {
+	int m = 0;
+	for (int tmp_size = size; tmp_size > 1; tmp_size /= 2, m++);
+	for (int p = 0; p < m; p++)
+	{
+		int butterflyOffset = 1 << (p + 1);
+		int butterflySize = butterflyOffset >> 1;
+		double coeff = PI / butterflySize;
+		for (int i = 0; i < size / butterflyOffset; i++)
+			for (int j = 0; j < butterflySize; j++)
+				Butterfly(signal, complex<double>(cos(-j * coeff),
+					sin(-j * coeff)), j + i * butterflyOffset, butterflySize);
+	}
+}
+
+// S E R I A L      FFT computation
+void SerialFFT(complex<double> *inputSignal,
+	complex<double> *outputSignal, int size) {
+	BitReversing(inputSignal, outputSignal, size);
+	SerialFFTCalculation(outputSignal, size);
+}
+
+void TestResult(complex<double> *inputSignal,
+	complex<double> *outputSignal, int size)
+{
+	// Buffer for storing the result of serial FFT
+	complex<double> *testSerialSignal;
+	double Accuracy = 1.e-6; // Comparison accuracy
+	// Flag, that shows whether the vectors are identical
+	int equal = 0;
+	int i; // Loop variable
+	testSerialSignal = new complex<double>[size];
+	SerialFFT(inputSignal, testSerialSignal, size);
+	for (i = 0; i < size; i++)
+	{
+		if (abs(outputSignal[i] - testSerialSignal[i]) >= Accuracy)
+			equal = 1;
+	}
+	if (equal == 1)
+		printf("The results of serial and parallel algorithms are NOT identical.Check your code.");
+	else
+		printf("The results of serial and parallel algorithms are identical.");
+	delete[] testSerialSignal;
+}
+
+double sumAn(double t, double T) {
+	double eps = 2e-8;
+	double sumIter = 1;
+	double k = 1;
+
+	double res = 0;
+	while (fabs(sumIter) > eps) {
+		sumIter = cos(k * 2 * PI / T * t) / k;
+		res += sumIter;
+		k++;
+	}
+	return res;
+}
+
+double fun(complex<double>* outputSignal, int size, double t, double T) {
+	double f = outputSignal[0].real() / 2;
+	for (int i = 1; i < size / 2; i++) {
+		f += outputSignal[i].real() * cos(i * 2 * PI * t / T) + outputSignal[i].imag() * sin(i * 2 * PI * t / T);
+	}
+	return f;
+}
+
+// Возвращает минимальную
+void Task9(complex<double>* inputSignal, complex<double>* outputSignal, int size, int T) {
+	inputSignal		= new complex<double>[size];
+	outputSignal	= new complex<double>[size];
+	// от 0 или 1?
+	for (int i = 1; i < size; i++) {
+		double t = i * 1.0 / size;
+		inputSignal[i] = complex<double>(-log(2 * sin(PI / T * t)), 0);
+	}
+
+	ParallelFFT(inputSignal, outputSignal, size);
+	for (int i = 0; i < size; i++) {
+		outputSignal[i] /= size / 2.;
+	}
+
+	for (int i = 0; i < 64; i++) {
+		double t = i / double(size);
+		cout << fun(outputSignal, size, t, T) << " --- " 
+			 << sumAn(t, T) << " --- " << -log(2.0 * sin(PI * t / T)) << endl;
+	}
+
+
+}
+
+int main()
+{
+	complex<double> *inputSignal = NULL;
+	complex<double> *outputSignal = NULL;
+	int size = 1024;
+	double T = 1;
+
+	Task9(inputSignal, outputSignal, size, T);
+	/*
+	int size = 0;
+	const int repeatCount = 16;
+	double startTime;
+	double duration;
+	double minDuration = DBL_MAX;
+	cout << "Fast Fourier Transform" << endl;
+	// Memory allocation and data initialization
+	ProcessInitialization(inputSignal, outputSignal, size);
+
+	for (int i = 0; i < repeatCount; i++)
+	{
+		startTime = clock();
+		// FFT computation
+		ParallelFFT(inputSignal, outputSignal, size);
+		duration = (clock() - startTime) / CLOCKS_PER_SEC;
+		if (duration < minDuration)
+			minDuration = duration;
+	}
+	
+	TestResult(inputSignal, outputSignal, size);
+	cout << setprecision(6);
+	cout << "\nExecution time is " << minDuration << " s. " << endl;
+	*/
+	// Result signal output
+	//PrintSignal(outputSignal, size);
+	// Computational process termination
+	ProcessTermination(inputSignal, outputSignal);
+	system("pause");
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
